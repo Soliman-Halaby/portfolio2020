d1875e3c899c3d484e413021808d11d9967feefe
Soliman-beep <60599592+Soliman-beep@users.noreply.github.com>
2020-07-12 22:51:20 +0200

    Updates

---
+++ precache-manifest.4d781cbf3db88eb6b34b818e27b77529.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f189b11d9a3b04c7cff53309598f107",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a38a6c215ecfea5441ee",
+    "url": "/portfolio/static/css/main.a8b9b552.chunk.css"
+  },
+  {
+    "revision": "10aa6a64a39317f5499a",
+    "url": "/portfolio/static/js/2.c4804db5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c4804db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38a6c215ecfea5441ee",
+    "url": "/portfolio/static/js/main.9ac73e2b.chunk.js"
+  },
+  {
+    "revision": "f73cb6802a5442b1ce5a",
+    "url": "/portfolio/static/js/runtime-main.7dc54a26.js"
+  },
+  {
+    "revision": "10b94d27c8cdf04fed921729df567348",
+    "url": "/portfolio/static/media/firstProject.10b94d27.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ffa0ba974f7d2fef582c6f64dec94c2",
+    "url": "/portfolio/static/media/phone.9ffa0ba9.svg"
+  }
+]);
