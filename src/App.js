940194e60c82443af3a7191a9110d42412649192
Soliman-beep <60599592+Soliman-beep@users.noreply.github.com>
2020-07-19 16:02:04 +0200

    Upload first version

--- src/App.js
+++ src/App.js
@@ -1,49 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import phone from './images/phone.svg'
+import style from './App.css'
+import project from './images/drumpad.png'
+import cv from './images/CV_Soliman_Al_Halaby.pdf'
 
 function App() {
   return (
-    <div className="container">
-      <div className="gallerie">
+  <div className="pageContainer">
+    <div className="gallerie">
+      <div class="cardContainer">
         <div className="card">
-          <div className="info">
-            <div className="titleInfo">
-              <h1 className="title">DrumPad</h1>
-              <div className="pictoContainer">
-                <div className="titlePicto">
-                  <img class="phoneResponsive" src={phone} alt="Responsive Phone"></img>
-                </div>
-                <div className="titlePicto">
-                  <img class="phoneResponsive" src={phone} alt="Responsive Phone"></img>
+          <a class="projectLink" href="https://soliman-halaby.github.io/drumPad/" >
+            <img className="projectImg" src={project} alt="Project representation"/>
+          </a>
+          <div className="cardTitle">
+            <img className="responsivePhone" id="greenColor" src={phone} alt="Phone responsive"></img>
+            <img className="responsiveComputer" id="greenColor" src={phone} alt="Phone responsive"></img>
+            <a class="projectTitle" href="https://soliman-halaby.github.io/drumPad/">Drumpad</a>
+          </div>
+          <div className="cardDate">Fait le 12/05/2020</div>
+          <div className="cardDescription">
+            Seul, réaliser un Drum Pad en JS. Avoir plusieurs sons et les contrôler + animations. Noté sur l'aspect, le respect des consignes, les features, la qualité et téchnicité du code.
+          </div>
+          <div className="cardBottom">
+            <div className="containerDetail">
+              <div className="oneThird">
+                <div className="cardGrade">Note : <span class="grade">20</span></div>
+              </div>
+              <div className="twoThird">
+                <div className="madeFor">Fais pour :
+                  <a className="name" href="https://github.com/brunosimon" target="_blank">Bruno Simon</a>
                 </div>
               </div>
             </div>
-            <span className="smallTitle">Projet Individuel</span>
-              <h3 className="innerTitle">Consignes :</h3>
-              <p className="description">Seul, réaliser un Drum Pad en JS. Avoir plusieurs sons et les contrôler + animations. Noté sur l'aspect, le respect des consignes, les features, la qualité et téchnicité du code.</p>
-              <span className="madeFor">Fais pour : <a className="forWho" href="https://github.com/brunosimon">Bruno Simon</a></span>
-              <div className="gradeContainer">
-                <span className="madeFor">Note : <span className="forWho">17</span></span>
-                <div class="detail">Détail</div>
-              </div>
-              <div className="boxGrade">
-                <span className="detailText">Consignes : <span className="myGrade">3</span><span className="detailGrade">/1</span></span>
-                <span className="detailText">Features : <span className="myGrade">3</span><span className="detailGrade">/6</span></span>
-                <span className="detailText">Aspect : <span className="myGrade">3</span><span className="detailGrade">/3</span></span>
-                <span className="detailText">Code : <span className="myGrade">3</span><span className="detailGrade">/5</span></span>
-                <span className="detailText">Technicité : <span className="myGrade">3</span><span className="detailGrade">/5</span></span>
-              </div>
-              <div className="bottomCard">
-                <span className="smallTitle">Fait le 07/02/20</span>
-                <a className="forWho" href="https://github.com/Soliman-Halaby/drumPad"><span className="smallTitle">Voir le projet</span></a>
-              </div>
-          </div>
-        </div>
+            <div className="containerBottom">
+              <div className="madeWith">Fais avec :</div>
+              <a className="participant participant1" target="_blank" href="#">Coucou</a>
+              <a className="participant participant2" target="_blank" href="#">Coucou</a>
+              <a className="participant participant3" target="_blank" href="#">Coucou</a>
+            </div>
+          </div> 
+        </div>   
       </div>
     </div>
+    <footer className="footer">
+      <span className="download">Télécharger</span><a className="cv" href={cv} target="_blank">mon CV</a>
+    </footer>
+  </div>
   );
 }
 
 export default App;
+
+
