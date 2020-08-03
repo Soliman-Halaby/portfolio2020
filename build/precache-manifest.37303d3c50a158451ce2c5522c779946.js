b75db1cfe7e9ed389206b9e251f482f0c655de9c
Soliman Al Halaby <60599592+Soliman-Halaby@users.noreply.github.com>
2020-08-03 22:50:43 +0200

    Upload

---
+++ build/precache-manifest.37303d3c50a158451ce2c5522c779946.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085aaa3003143e7eb8315718c15dc119",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3348c5770aacb15d8fbc",
+    "url": "/portfolio/static/css/main.2325a5bc.chunk.css"
+  },
+  {
+    "revision": "a6607c4db400be5dc5d7",
+    "url": "/portfolio/static/js/2.142a1c9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.142a1c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3348c5770aacb15d8fbc",
+    "url": "/portfolio/static/js/main.c4d49627.chunk.js"
+  },
+  {
+    "revision": "f73cb6802a5442b1ce5a",
+    "url": "/portfolio/static/js/runtime-main.7dc54a26.js"
+  },
+  {
+    "revision": "b2e49dce1ce347a633f73c890c7a1f00",
+    "url": "/portfolio/static/media/CV_Soliman_Al_Halaby.b2e49dce.pdf"
+  },
+  {
+    "revision": "9739f92233946479b72c4070d85791f2",
+    "url": "/portfolio/static/media/computer.9739f922.svg"
+  },
+  {
+    "revision": "afea90815bc97fd5eb4897298cb17149",
+    "url": "/portfolio/static/media/diceRoller.afea9081.png"
+  },
+  {
+    "revision": "10b94d27c8cdf04fed921729df567348",
+    "url": "/portfolio/static/media/drumpad.10b94d27.png"
+  },
+  {
+    "revision": "79ddd4cf229f3ec1db01e12cccfdd204",
+    "url": "/portfolio/static/media/gameBoy.79ddd4cf.png"
+  },
+  {
+    "revision": "182ae735d3bdf87aa9e48d7f52a442f4",
+    "url": "/portfolio/static/media/iconMoon.182ae735.svg"
+  },
+  {
+    "revision": "50e25aba07f73c3c8c0904a1e96ce3d7",
+    "url": "/portfolio/static/media/iconSunDark.50e25aba.svg"
+  },
+  {
+    "revision": "610398f47e8848d152f9a746e3605cd2",
+    "url": "/portfolio/static/media/milleBoards.610398f4.png"
+  },
+  {
+    "revision": "3e05145d62e4d9601b6d95b2f7ceafd5",
+    "url": "/portfolio/static/media/oralB.3e05145d.png"
+  },
+  {
+    "revision": "9ffa0ba974f7d2fef582c6f64dec94c2",
+    "url": "/portfolio/static/media/phone.9ffa0ba9.svg"
+  },
+  {
+    "revision": "20de1326d5a94d2195995a4ee161e544",
+    "url": "/portfolio/static/media/savoirMag.20de1326.png"
+  },
+  {
+    "revision": "1b274dd8269e2e80b38ea0eaf2fd42cc",
+    "url": "/portfolio/static/media/tremplinDigital.1b274dd8.png"
+  }
+]);
