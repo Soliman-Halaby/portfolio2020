e06fa13b331f749c8f146093f0296ade468c5e7f
Soliman Al Halaby <60599592+Soliman-Halaby@users.noreply.github.com>
2021-02-07 16:45:10 +0100

    Updates

---
+++ precache-manifest.895c34ccd130a73054abd3ee56c29c86.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce325a43e525be72fd1edd75d1100374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f26b814d3ac12ff05b6",
+    "url": "/static/css/main.98525889.chunk.css"
+  },
+  {
+    "revision": "5c35694fd17689ea8b46",
+    "url": "/static/js/2.7b61f233.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b61f233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f26b814d3ac12ff05b6",
+    "url": "/static/js/main.3df0820f.chunk.js"
+  },
+  {
+    "revision": "89fbb67437dabdd6325f",
+    "url": "/static/js/runtime-main.2be7f483.js"
+  },
+  {
+    "revision": "e30723065cddc01afb2cbb10c34f4a2d",
+    "url": "/static/media/CV_Soliman_Al_Halaby.e3072306.pdf"
+  },
+  {
+    "revision": "9739f92233946479b72c4070d85791f2",
+    "url": "/static/media/computer.9739f922.svg"
+  },
+  {
+    "revision": "afea90815bc97fd5eb4897298cb17149",
+    "url": "/static/media/diceRoller.afea9081.png"
+  },
+  {
+    "revision": "10b94d27c8cdf04fed921729df567348",
+    "url": "/static/media/drumpad.10b94d27.png"
+  },
+  {
+    "revision": "79ddd4cf229f3ec1db01e12cccfdd204",
+    "url": "/static/media/gameBoy.79ddd4cf.png"
+  },
+  {
+    "revision": "2afc1c117414f5479a2b930acd5ea79f",
+    "url": "/static/media/iconMoon.2afc1c11.svg"
+  },
+  {
+    "revision": "50e25aba07f73c3c8c0904a1e96ce3d7",
+    "url": "/static/media/iconSunDark.50e25aba.svg"
+  },
+  {
+    "revision": "610398f47e8848d152f9a746e3605cd2",
+    "url": "/static/media/milleBoards.610398f4.png"
+  },
+  {
+    "revision": "3e05145d62e4d9601b6d95b2f7ceafd5",
+    "url": "/static/media/oralB.3e05145d.png"
+  },
+  {
+    "revision": "9ffa0ba974f7d2fef582c6f64dec94c2",
+    "url": "/static/media/phone.9ffa0ba9.svg"
+  },
+  {
+    "revision": "20de1326d5a94d2195995a4ee161e544",
+    "url": "/static/media/savoirMag.20de1326.png"
+  },
+  {
+    "revision": "e5a539a7aeace23cde1775eef33d7561",
+    "url": "/static/media/showroom.e5a539a7.png"
+  },
+  {
+    "revision": "3edde32e5d7f94d176c5d6342c081378",
+    "url": "/static/media/unTronePoursuivi.3edde32e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe73ce75059d2977e2d31bde954fef83.js"
+  "/precache-manifest.895c34ccd130a73054abd3ee56c29c86.js"
 );
 
 self.addEventListener('message', (event) => {
